1a1d788481d5b6235d59ea2c9d6143f75de37c4f
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-03-10 02:36:44 +0300

    crated first API

---
+++ controllers/AppController.js
@@ -0,0 +1,20 @@
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+class AppController {
+  static async getStatus(req, res) {
+    const redisStatus = await redisClient.isAlive();
+    const dbStatus = await dbClient.isAlive();
+
+    return res.status(200).json({ redis: redisStatus, db: dbStatus });
+  }
+
+  static async getStats(req, res) {
+    const usersCount = await dbClient.nbUsers();
+    const filesCount = await dbClient.nbFiles();
+
+    return res.status(200).json({ users: usersCount, files: filesCount });
+  }
+}
+
+export default AppController;
