b394053ed42ba23fd10096149e27be2b79ea3c33
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2024-03-09 04:48:29 +0300

    created mongodb connection

---
+++ utils/db.js
@@ -0,0 +1,43 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const {
+      DB_HOST = 'localhost',
+      DB_PORT = 27017,
+      DB_DATABASE = 'files_manager',
+    } = process.env;
+
+    const url = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+
+    this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+    this.connect();
+  }
+
+  async connect() {
+    try {
+      await this.client.connect();
+    } catch (error) {
+      console.error(`Error connecting to MongoDB: ${error}`);
+    }
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    await this.connectionPromise;
+    const usersCollection = this.client.db().collection('users');
+    return usersCollection.countDocuments();
+  }
+
+  async nbFiles() {
+    await this.connectionPromise;
+    const filesCollection = this.client.db().collection('files');
+    return filesCollection.countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
